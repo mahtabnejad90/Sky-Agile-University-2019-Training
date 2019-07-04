200f1b0eab9b835b13f54f853d0ca6db723e534f
Unknown <mahtab.nejad@sky.uk>
2019-07-04 13:25:27 +0100

    Added javascript session 1 files. changing text field

---
+++ JS-Sesion-1/js-external-script.js
@@ -0,0 +1,9 @@
+//function penguinfunctions(changetext) {
+//document.getElementById('text_to_change').innerHTML = changetext;
+
+function greeting () {
+
+var name = document.getElementById('input_text').value;
+document.getElementById('text_to_change').innerHTML = 'My Name is, ' + name;
+		
+}
