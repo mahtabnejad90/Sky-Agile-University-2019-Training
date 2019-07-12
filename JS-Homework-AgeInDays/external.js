3d7c2ecd717f1b53f6d91687b3032094bf92efc6
Unknown <mahtab.nejad@sky.uk>
2019-07-12 10:47:21 +0100

    Homework-Session 2
    Uploaded days alive homework. Use of if & loops.

--- JS-Homework-AgeInDays/external.js
+++ JS-Homework-AgeInDays/external.js
@@ -7,4 +7,11 @@ function calculateDaysAlive() {
     var diffTime = Math.abs(todaysDate.getTime() - birthDate.getTime());
     var diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));    
     document.getElementById('output').innerHTML = `You are ${diffDays} days old!`;
+
+if(monthOfBirth < 0 || (monthOfBirth === 0 && todaysDate.getDate() < birthDate.getDate())){
+	myAge = myAge - 1;
+}
+
+return myAge;
+
 }
