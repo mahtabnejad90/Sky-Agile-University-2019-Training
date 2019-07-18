56c12ba393221c8789188a1065878c88f56f15db
Unknown <mahtab.nejad@sky.uk>
2019-07-18 13:43:26 +0100

    for loops
    for loops

--- Final-Session/js/myscript.js
+++ Final-Session/js/myscript.js
@@ -1,3 +1,17 @@
 function myFunc(){
 	console.log("Hi I am mahtab");
 }
+
+function calculateTotal() {
+	var numbersUpTo = document.getElementById('input_number').value;
+	var startingNumber;
+	var runningTotal = 0;
+
+
+	for(startingNumber = 0; startingNumber <= numbersUpTo; startingNumber++) {
+		runningTotal += startingNumber;
+	}
+
+document.getElementById('output_number').innerHTML = runningTotal;
+
+}
